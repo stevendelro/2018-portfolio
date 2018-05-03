f0c7b22e8a559c00cbd76576fa899b40eaa51748
stevendelro <stevendelro@gmail.com>
2018-05-03 08:50:27 -0700

    Added form and styling to contact page.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,63 +2,65 @@ import React from 'react';
 
 const ContactPage = () => (
   <div className="row">
-    <form className="col s12">
-      <div className="row">
-        <div className="input-field col s6">
-          <input
-            placeholder="Placeholder"
-            id="first_name"
-            type="text"
-            className="validate"
-          />
-          <label htmlFor="first_name">First Name</label>
+    <div className="col s12 m2 l2" />
+    <div className="col s12 m8 l8" style={{ paddingTop: 3 + 'rem' }}>
+      <div className="card hoverable">
+        <div className="card-image">
+          <img src="https://images.unsplash.com/photo-1521189370969-961c2262630a?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=64a821d32dceaa2037ed0b472218b405&auto=format&fit=crop&w=2068&q=80" />
+          <span className="card-title">
+            <h1 className='white-text-shadow'>Contact</h1>
+          </span>
+          <a className="btn-floating halfway-fab waves-effect waves-light red">
+            <i className="far fa-paper-plane" />
+          </a>
         </div>
-        <div className="input-field col s6">
-          <input id="last_name" type="text" className="validate" />
-          <label htmlFor="last_name">Last Name</label>
-        </div>
-      </div>
-      <div className="row">
-        <div className="input-field col s12">
-          <input
-            disabled
-            value="I am not editable"
-            id="disabled"
-            type="text"
-            className="validate"
-          />
-          <label htmlFor="disabled">Disabled</label>
-        </div>
-      </div>
-      <div className="row">
-        <div className="input-field col s12">
-          <input id="password" type="password" className="validate" />
-          <label htmlFor="password">Password</label>
-        </div>
-      </div>
-      <div className="row">
-        <div className="input-field col s12">
-          <input id="email" type="email" className="validate" />
-          <label htmlFor="email">Email</label>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col s12">
-          This is an inline input field:
-          <div className="input-field inline">
-            <input id="email_inline" type="email" className="validate" />
-            <label htmlFor="email_inline">Email</label>
-            <span
-              className="helper-text"
-              data-error="wrong"
-              data-success="right"
-            >
-              Helper text
-            </span>
+        <div className="card-content">
+          <div className="row">
+            <form className="col s12">
+              <div className="row">
+                <div className="input-field col s6">
+                  <input id="first_name" type="text" className="validate" />
+                  <label htmlFor="first_name">First Name</label>
+                </div>
+                <div className="input-field col s6">
+                  <input id="last_name" type="text" className="validate" />
+                  <label htmlFor="last_name">Last Name</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s6">
+                  <input type="text" className="validate" />
+                  <label htmlFor="company">Company</label>
+                </div>
+                <div className="input-field col s6">
+                  <input type="text" className="validate" />
+                  <label htmlFor="position">Position</label>
+                </div>
+              </div>
+              <div className="row">
+                <div className="input-field col s6">
+                  <input id="email" type="email" className="validate" />
+                  <label htmlFor="email">Email</label>
+                </div>
+                <div className="input-field col s6">
+                  <input type="text" className="validate" />
+                  <label htmlFor="phone">Phone</label>
+                </div>
+                <div className="col s12">
+                  <div className="row">
+                    <div className="input-field col s12">
+                      <textarea className="materialize-textarea" />
+                      <label htmlFor="message">Message (optional)</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </form>
           </div>
         </div>
       </div>
-    </form>
+    </div>
+    <div className="col s12 m2 l2" />
   </div>
 );
 
