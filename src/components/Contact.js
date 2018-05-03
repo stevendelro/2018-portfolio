79b513a59c2523f1741c980fbf6f339854307a5f
stevendelro <stevendelro@gmail.com>
2018-05-03 06:22:51 -0700

    Test contact form js functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,65 @@
 import React from 'react';
 
-const ContactPage = () => <div>This is my CONTACT component.</div>;
+const ContactPage = () => (
+  <div className="row">
+    <form className="col s12">
+      <div className="row">
+        <div className="input-field col s6">
+          <input
+            placeholder="Placeholder"
+            id="first_name"
+            type="text"
+            className="validate"
+          />
+          <label htmlFor="first_name">First Name</label>
+        </div>
+        <div className="input-field col s6">
+          <input id="last_name" type="text" className="validate" />
+          <label htmlFor="last_name">Last Name</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <input
+            disabled
+            value="I am not editable"
+            id="disabled"
+            type="text"
+            className="validate"
+          />
+          <label htmlFor="disabled">Disabled</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <input id="password" type="password" className="validate" />
+          <label htmlFor="password">Password</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="input-field col s12">
+          <input id="email" type="email" className="validate" />
+          <label htmlFor="email">Email</label>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col s12">
+          This is an inline input field:
+          <div className="input-field inline">
+            <input id="email_inline" type="email" className="validate" />
+            <label htmlFor="email_inline">Email</label>
+            <span
+              className="helper-text"
+              data-error="wrong"
+              data-success="right"
+            >
+              Helper text
+            </span>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+);
 
 export default ContactPage;
