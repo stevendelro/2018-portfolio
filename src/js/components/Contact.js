975f7c42ee9b673421ab4a2379855f59d7a4f429
Steven Del Rosario <stevendelro@pm.me>
2018-11-01 15:30:12 -0700

    Set up basic controlled inputs for input fields.

--- src/js/components/Contact.js
+++ src/js/components/Contact.js
@@ -1,61 +1,93 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Contact = () => (
-  <section className="section-contact" id="contact">
-    <div className="row">
-      <div className="contact">
-        <div className="contact__form">
-          <form action="#" className="form">
-            <div className="u-margin-bottom-small-1">
-              <h2 className="heading-secondary heading-secondary--white">
-                contact
-              </h2>
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      message: ''
+    }
+    this.handleName = this.handleName.bind(this);
+    this.handleEmail = this.handleEmail.bind(this);
+    this.handleMessage = this.handleMessage.bind(this);
+  }
+
+  handleName(event) {
+    this.setState({ name: event.target.value })
+  }
+  handleEmail(event) {
+    this.setState({ email: event.target.value })
+  }
+  handleMessage(event) {
+    this.setState({ message: event.target.value })
+  }
+  render() {
+    return (
+      <section className="section-contact" id="contact">
+        <div className="row">
+          <div className="contact">
+            <div className="contact__form">
+              <form action="#" className="form">
+                <div className="u-margin-bottom-small-1">
+                  <h2 className="heading-secondary heading-secondary--white">
+                    contact
+                  </h2>
+                </div>
+                <div className="form__group">
+                  <input
+                    type="text"
+                    className="form__input"
+                    placeholder="FULL NAME"
+                    id="name"
+                    value={this.state.name}
+                    onChange={this.handleName}
+                    required
+                  />
+                  <label htmlFor="name" className="form__label">
+                    FULL NAME
+                  </label>
+                </div>
+                <div className="form__group">
+                  <input
+                    type="email"
+                    className="form__input"
+                    placeholder="EMAIL"
+                    id="email"
+                    value={this.state.email}
+                    onChange={this.handleEmail}
+                    required
+                  />
+                  <label htmlFor="email" className="form__label">
+                    EMAIL
+                  </label>
+                </div>
+                <div className="form__group">
+                  <textarea
+                    name="message"
+                    maxLength="1000"
+                    form="form"
+                    rows="7"
+                    className="form__input"
+                    placeholder="MESSAGE"
+                    value={this.state.message}
+                    onChange={this.handleMessage}
+                    id="message"
+                  />
+                </div>
+                <div className="form__group">
+                  <button className="btn btn--primary u-margin-top-small">
+                    SEND
+                  </button>
+                </div>
+              </form>
             </div>
-            <div className="form__group">
-              <input
-                type="text"
-                className="form__input"
-                placeholder="FULL NAME"
-                id="name"
-                required
-              />
-              <label htmlFor="name" className="form__label">
-                FULL NAME
-              </label>
-            </div>
-            <div className="form__group">
-              <input
-                type="email"
-                className="form__input"
-                placeholder="EMAIL"
-                id="email"
-                required
-              />
-              <label htmlFor="email" className="form__label">
-                EMAIL
-              </label>
-            </div>
-            <div className="form__group">
-              <textarea
-                name="message"
-                maxLength="1000"
-                form="form"
-                rows="7"
-                className="form__input"
-                placeholder="MESSAGE"
-                id="message"
-              />
-            </div>
-            <div className="form__group">
-              <button className="btn btn--primary u-margin-top-small">
-                SEND
-              </button>
-            </div>
-          </form>
+          </div>
         </div>
-      </div>
-    </div>
-  </section>
-);
+      </section>
+    );
+  }
+}
 
 export default Contact;
