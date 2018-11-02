788af8c236a98650a029f17c13d37645aaa05d28
Steven Del Rosario <stevendelro@pm.me>
2018-11-01 17:04:29 -0700

    POST requests to "/" will now use SendGrid to send emails.

--- server/server.js
+++ server/server.js
@@ -2,8 +2,11 @@ require('babel-polyfill');
 const favicon = require('serve-favicon');
 const express = require('express');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const axios = require('axios');
 const path = require('path');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const app = express();
 const public = path.join(__dirname, '..', 'public');
@@ -26,9 +29,15 @@ app.use(morgan('dev'));
 app.use(favicon(icon));
 app.use(express.static(dist));
 app.use(express.static(public));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(public, 'index.html'));
+});
+
+app.post('/', (req, res) => {
+  sgMail.send(req.body);
 });
 
 module.exports = app;
