93886475b420377fb16f1e25b23662e515cbfb80
Steven Del Rosario <stevendelro@pm.me>
2018-11-05 15:39:21 -0800

    Removed attribute typo.

--- src/js/pages/PageNotFound.js
+++ src/js/pages/PageNotFound.js
@@ -4,7 +4,7 @@ import { Link } from 'preact-router';
 const PageNotFound = ({ type, url }) => (
 	<section className="page-404">
 		<h1>Error {type}</h1>
-		<h3 sty>Page not found!</h3>
+		<h3>Page not found!</h3>
 		<pre>Route {url} doesn't exist.</pre>
     <Link href="/">Go back</Link>
 	</section>
